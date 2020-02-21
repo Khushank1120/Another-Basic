ee4cd57a8cc8b99f52d68609a761660bccff0778
Khushank <47456301+Khushank2001@users.noreply.github.com>
2020-02-21 20:05:23 +0530

    Add files via upload

---
+++ Day5Loops.java
@@ -0,0 +1,28 @@
+package thirtyDaysOfCode;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day5Loops {
+
+	public static class Solution {
+
+
+	    public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int n = in.nextInt();
+	        in.close();
+	        
+	        for(int i = 1; i <= 10; i++){
+	            System.out.println(n + " x " + i + " = " + n * i);
+	        }
+	    }
+	}
+
+
+
+}
---
+++ Day6LetsReview.java
@@ -0,0 +1,37 @@
+package thirtyDaysOfCode;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.*;
+
+public class Day6LetsReview {
+
+
+	    public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        int testCases = scan.nextInt();
+	        for(int i = 0; i < testCases; i++){
+	            char[] inputString = scan.next().toCharArray();
+	            
+	            // Print even chars
+	            for(int j = 0; j < inputString.length; j += 2){
+	                System.out.print(inputString[j]);
+	            }
+	            System.out.print(" ");
+	            
+	            // Print odd chars
+	            for(int j = 1; j < inputString.length; j += 2){
+	                System.out.print(inputString[j]);
+	            }
+	            System.out.println();
+	        }
+	        scan.close();
+	    }
+	}
+
+
+
+
+
