93c940d1b74a156576f37cd5a71a2f04790aada5
Khushank <47456301+Khushank2001@users.noreply.github.com>
2020-02-21 20:04:53 +0530

    Add files via upload

---
+++ Day2Operators.java
@@ -0,0 +1,29 @@
+package thirtyDaysOfCode;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day2Operators {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+        // Calculate Tax and Tip:
+        double tip = mealCost * tipPercent / 100;
+        double tax = mealCost * taxPercent / 100;
+        
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(mealCost + tax + tip);
+      
+        System.out.println( totalCost );
+    }
+}
